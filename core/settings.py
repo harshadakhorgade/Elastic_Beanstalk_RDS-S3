7cdb81b1b481e0eeb822e3ce85c69e2a327bfabb
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-04-08 15:03:28 +0530

    added

--- core/settings.py
+++ core/settings.py
@@ -136,6 +136,10 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
     
     # Use AWS_REGION if S3 region is not explicitly defined
     AWS_REGION = os.environ.get('AWS_REGION', 'us-west-2')
