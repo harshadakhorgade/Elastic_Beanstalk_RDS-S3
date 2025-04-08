ed583bbf26ead41953b8cbf4d8e1617f4deb2114
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-04-08 14:22:25 +0530

    added

--- core/settings.py
+++ core/settings.py
@@ -144,6 +144,8 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_LOCATION = 'media'
 
+
+    AWS_DEFAULT_ACL = 'public-read'
     # Static files (CSS, JS)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
