1f99b5eb8a078559d9ae07295c6fe9a41f776fc9
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-04-10 12:48:41 +0530

    updated

--- core/settings.py
+++ core/settings.py
@@ -109,12 +109,12 @@ DATABASES = {
 #         }
 #     }
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 print("Using DB:", DATABASES['default'])
 
